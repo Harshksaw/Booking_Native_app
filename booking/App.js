63cb04926a2ccac0a061a9c0c6c8fb599909f36e
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-04-05 16:26:40 +0000

    Init Booking App

---
+++ booking/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  function BottomTabs(){
+    return(
+    <Tab.Navigator>
+    )
+  }
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
