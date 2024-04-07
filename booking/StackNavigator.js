ea16aec9a9fe64e1fa8322c84da2331bcfd6f0e3
Harsh kumar <mister.harshkumar@gmail.com>
2024-04-07 17:55:31 +0530

    Add header component to HomeScreen, And Vector icons

--- booking/StackNavigator.js
+++ booking/StackNavigator.js
@@ -97,7 +97,7 @@ const StackNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={BottomTabs} />
+        <Stack.Screen name="Main" component={BottomTabs}  options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- booking/screens/HomeScreen.js
+++ booking/screens/HomeScreen.js
@@ -1,10 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
+import {useNavigation} from "@react-navigation/native"
+import { Ionicons } from "@expo/vector-icons";
+import Header from '../components/Header';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "Booking.com",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+        borderBottomColor: "transparent",
+        shadowColor:"transparent"
+      },
+      headerStyle: {
+        backgroundColor: "#003580",
+        height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
+      },
+      headerRight: () => (
+        <Ionicons
+          name="notifications-outline"
+          size={24}
+          color="white"
+          style={{ marginRight: 12 }}
+        />
+      ),
+    });
+  }, []);
+
   return (
     <View>
-      <Text>HomeScreen</Text>
+        <Header/>
     </View>
   )
 }
